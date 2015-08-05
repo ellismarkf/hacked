563f271588cc2e9295d423f1d6e43fca1b993cb4
Mark F. Ellis II <mellis@pros.com>
2015-08-04 22:24:13 -0500

    use jQuery terminal library

--- src/js/termulator.js
+++ src/js/termulator.js
@@ -1,49 +1,21 @@
-;(function(Termulator) {
-    function Termulator(options) {
-        var self = this;
-
-        if (!(this instanceof Termulator)) {
-            return new Termulator(arguments[0], arguments[1], arguments[2]);
-        }
-
-        if (typeof options === 'number') {
-            options = {
-                cols: arguments[0],
-                rows: arguments[1],
-                container: arguments[2]
-            };
-        }
-
-        options = options || {};
-
-        this.cols = options.cols || Termulator.defaults.cols;
-        this.rows = options.rows || Termulator.defaults.cols;
-        this.commands = options.commands || Termulator.defaults.commands;
+function parseCommand (arguments) {
+    var command = {
+        command: arguments[0]
+    }
+}
+$('#terminal').terminal({
+    help: function(arg1) {
+        this.echo('help menu ' + arg1);
+    },
+    ping: function(ip) {
 
     }
-
-    Termulator.defaults = {
-        cols: 80,
-        rows: 50,
-        commands: ['help']
-
-    }
-
-    Termulator.prototype.handlers = function(commands) {
-        var i;
-        for (i = 0; i < commands.length; i++) {
-            console.log(commands[i]);
-        }
-    };
-
-    Termulator.prototype.draw = function(container) {
-        container = window.document.getElementById(container);
-        container.classList.add('terminalWindow');
-        container.style.width = this.cols + 'px';
-        debugger;
-    };
-    this.Termulator = Termulator;
-
-}).call(function() {
-    return this || (typeof window !== 'undefined' ? window : global);
-}());
+}, {
+    prompt: '> ',
+    greetings: '193u4.194u234lk login successful',
+    name: 'main-window',
+    history: true,
+    height: 500,
+    width: 400,
+    checkArity: false
+});
